b05ae1d10580e2087c0eaafdead0cb3aa5f6c6e0
Knastoron <bene.schlai@gmx.de>
2021-01-22 20:07:17 +0100

    Basic functionality, interpolation for px-coordinates not yet implemented

--- ArcGIS GCP Calculation/MainWindow.xaml.cs
+++ ArcGIS GCP Calculation/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +23,168 @@ namespace ArcGIS_GCP_Calculation
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Filepaths for
+        public string 
+            TopLeftStart = string.Empty, 
+            TopRightStart = string.Empty, 
+            BottomLeftStart = string.Empty, 
+            BottomRightStart = string.Empty;
+
+        public int MapScopeX = 1100;
+        public int MapScopeY = 1100;
+        public readonly string FileNameExport = "ControlPoints_";
+
+        private void Calculate(object sender, RoutedEventArgs e)
+        {
+            if(TopLeftStart != string.Empty &&
+                TopRightStart != string.Empty &&
+                BottomLeftStart != string.Empty &&
+                BottomRightStart != string.Empty)
+            {
+                DoSomeCalculation();
+            }
+            else
+            {
+                 MessageBox.Show("Please add a file for each corner.");
+            }
+        }
+
+        public void DoSomeCalculation()
+        {
+            MapObject topLeft = ExtractData(TopLeftStart),
+                bottomLeft = ExtractData(BottomLeftStart),
+                bottomRight = ExtractData(BottomRightStart),
+                topRight = ExtractData(TopRightStart);
+
+            int width = (int)Math.Abs((topRight.TopLeftX - topLeft.TopLeftX)) / MapScopeX ;
+            int height = (int)Math.Abs((topLeft.TopLeftY - bottomLeft.TopLeftY)) / MapScopeY ;
+
+            MapObject[,] DoomArray = new MapObject[width+1,height+1];
+            DoomArray[0, 0] = bottomLeft;
+            DoomArray[0, height] = topLeft;
+            DoomArray[width, 0] = bottomRight;
+            DoomArray[width, height] = topRight;
+
+
+            for (int x = 0; x <= width; x++)
+            {
+                for(int y = 0; y<= height; y++)
+                {
+                    if(DoomArray[x, y] == null)
+                    {
+                    DoomArray[x, y] = DoomArray[0,0].ReturnCopyOfMe();
+                    DoomArray[x, y].ApplyXModifier(MapScopeX * x);
+                    DoomArray[x, y].ApplyYModifier(MapScopeY * y);
+                    }
+                }
+
+            }
+
+
+
+
+
+
+            //Dump in FIles
+            int count = 1;
+            Directory.CreateDirectory("Exports");
+            var filepaths = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            for (int y = height; y >=0 ; y--)
+            {
+                for (int x = 0; x <= width; x++)
+                {
+                    if (DoomArray[x, y] !=null)
+                    {
+                        var filename = filepaths+"/Exports/" +FileNameExport + count.ToString("0000")+".txt";
+                        File.WriteAllText(filename, DoomArray[x, y].DumpToFile());
+                        count++;
+                    }
+                }
+
+            }
+        }
+
+        public MapObject ExtractData(string filePath)
+        {
+            var ReturnObject = new MapObject();
+            using (var reader = new StreamReader(filePath))
+            {
+                var coords = reader.ReadLine().Split('\t');
+                ReturnObject.PxTopLeftX = double.Parse(coords[0], CultureInfo.InvariantCulture);
+                ReturnObject.PxTopLeftY = double.Parse(coords[1], CultureInfo.InvariantCulture);
+                ReturnObject.TopLeftX = double.Parse(coords[2], CultureInfo.InvariantCulture);
+                ReturnObject.TopLeftY = double.Parse(coords[3], CultureInfo.InvariantCulture);
+
+                coords = reader.ReadLine().Split('\t');
+                ReturnObject.PxBottomLeftX = double.Parse(coords[0], CultureInfo.InvariantCulture);
+                ReturnObject.PxBottomLeftY = double.Parse(coords[1], CultureInfo.InvariantCulture);
+                ReturnObject.BottomLeftX = double.Parse(coords[2], CultureInfo.InvariantCulture);
+                ReturnObject.BottomLeftY = double.Parse(coords[3], CultureInfo.InvariantCulture);
+
+                coords = reader.ReadLine().Split('\t');
+                ReturnObject.PxBottomRightX = double.Parse(coords[0], CultureInfo.InvariantCulture);
+                ReturnObject.PxBottomRightY = double.Parse(coords[1], CultureInfo.InvariantCulture);
+                ReturnObject.BottomRightX = double.Parse(coords[2], CultureInfo.InvariantCulture);
+                ReturnObject.BottomRightY = double.Parse(coords[3], CultureInfo.InvariantCulture);
+
+                coords = reader.ReadLine().Split('\t');
+                ReturnObject.PxTopRightX = double.Parse(coords[0], CultureInfo.InvariantCulture);
+                ReturnObject.PxTopRightY = double.Parse(coords[1], CultureInfo.InvariantCulture);
+                ReturnObject.TopRightX = double.Parse(coords[2], CultureInfo.InvariantCulture);
+                ReturnObject.TopRightY = double.Parse(coords[3], CultureInfo.InvariantCulture);
+
+                coords = reader.ReadLine().Split('\t');
+                ReturnObject.PxMiddleX = double.Parse(coords[0], CultureInfo.InvariantCulture);
+                ReturnObject.PxMiddleY = double.Parse(coords[1], CultureInfo.InvariantCulture);
+                ReturnObject.MiddleX = double.Parse(coords[2], CultureInfo.InvariantCulture);
+                ReturnObject.MiddleY = double.Parse(coords[3], CultureInfo.InvariantCulture);
+
+
+            }
+            return ReturnObject;
+        }
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnDrag(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // Note that you can have more than one file.
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                // Assuming you have one file that you care about, pass it off to whatever
+                // handling code you have defined.
+                Console.WriteLine(files[0].ToString());
+                Console.WriteLine(((Rectangle)sender).Name);
+                var s = ((Rectangle)sender);
+                switch (s.Name)
+                {
+                    case "BottomLeft":
+                        BottomLeftStart = files[0].ToString();
+                        s.Fill = new SolidColorBrush(Color.FromRgb(127, 255, 0));
+                        break;
+
+                    case "BottomRight":
+                        BottomRightStart = files[0].ToString();
+                        s.Fill = new SolidColorBrush(Color.FromRgb(127, 255, 0));
+                        break;
+
+                    case "TopLeft":
+                        TopLeftStart = files[0].ToString();
+                        s.Fill = new SolidColorBrush(Color.FromRgb(127, 255, 0));
+                        break;
+
+                    case "TopRight":
+                        TopRightStart = files[0].ToString();
+                        s.Fill = new SolidColorBrush(Color.FromRgb(127, 255, 0));
+                        break;
+                }
+            }
         }
     }
 }
---
+++ ArcGIS GCP Calculation/MapObject.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcGIS_GCP_Calculation
+{
+    public class MapObject
+    {
+        public double TopLeftX = 0;
+        public double TopRightX = 0;
+        public double MiddleX = 0;
+        public double BottomLeftX = 0;
+        public double BottomRightX = 0;
+
+        public double TopLeftY = 0;
+        public double TopRightY = 0;
+        public double MiddleY = 0;
+        public double BottomLeftY = 0;
+        public double BottomRightY = 0;
+
+
+
+
+        public double PxTopLeftX = 0;
+        public double PxTopRightX = 0;
+        public double PxMiddleX = 0;
+        public double PxBottomLeftX = 0;
+        public double PxBottomRightX = 0;
+        
+        public double PxTopLeftY = 0;
+        public double PxTopRightY = 0;
+        public double PxMiddleY = 0;
+        public double PxBottomLeftY = 0;
+        public double PxBottomRightY = 0;
+        public MapObject ReturnCopyOfMe()
+        {
+            var copy = new MapObject();
+
+            copy.TopLeftX = TopLeftX;
+            copy.TopRightX = TopRightX;
+            copy.MiddleX = MiddleX;
+            copy.BottomLeftX = BottomLeftX;
+            copy.BottomRightX = BottomRightX;
+
+            copy.TopLeftY = TopLeftY;
+            copy.TopRightY = TopRightY;
+            copy.MiddleY = MiddleY;
+            copy.BottomLeftY = BottomLeftY;
+            copy.BottomRightY = BottomRightY;
+
+            copy.PxTopLeftX = PxTopLeftX;
+            copy.PxTopRightX = PxTopRightX;
+            copy.PxMiddleX = PxMiddleX;
+            copy.PxBottomLeftX = PxBottomLeftX;
+            copy.PxBottomRightX = PxBottomRightX;
+
+            copy.PxTopLeftY = PxTopLeftY;
+            copy.PxTopRightY = PxTopRightY;
+            copy.PxMiddleY = PxMiddleY;
+            copy.PxBottomLeftY = PxBottomLeftY;
+            copy.PxBottomRightY = PxBottomRightY;
+
+
+            return copy;
+        }
+        public void ApplyXModifier(int additive)
+        {
+            TopLeftX += additive;
+            TopRightX += additive;
+            MiddleX += additive;
+            BottomLeftX += additive;
+            BottomRightX += additive;
+        }
+        public void ApplyYModifier(int additive)
+        {
+            TopLeftY += additive;
+            TopRightY += additive;
+            MiddleY += additive;
+            BottomLeftY += additive;
+            BottomRightY += additive;
+        }
+        public string DumpToFile()
+        {
+            StringBuilder text = new StringBuilder();
+            text.AppendLine(PxTopLeftX.ToString("F6", CultureInfo.InvariantCulture) + '\t' + PxTopLeftY.ToString("F6", CultureInfo.InvariantCulture) + '\t' + TopLeftX.ToString("F6", CultureInfo.InvariantCulture) + '\t' + TopLeftY.ToString("F6", CultureInfo.InvariantCulture));
+            text.AppendLine(PxBottomLeftX.ToString("F6", CultureInfo.InvariantCulture) + '\t' + PxBottomLeftY.ToString("F6", CultureInfo.InvariantCulture) + '\t' + BottomLeftX.ToString("F6", CultureInfo.InvariantCulture) + '\t' + BottomLeftY.ToString("F6", CultureInfo.InvariantCulture));
+            text.AppendLine(PxBottomRightX.ToString("F6", CultureInfo.InvariantCulture) + '\t' + PxBottomRightY.ToString("F6", CultureInfo.InvariantCulture) + '\t' + BottomRightX.ToString("F6", CultureInfo.InvariantCulture) + '\t' + BottomRightY.ToString("F6", CultureInfo.InvariantCulture));
+            text.AppendLine(PxTopRightX.ToString("F6", CultureInfo.InvariantCulture) + '\t' + PxTopRightY.ToString("F6", CultureInfo.InvariantCulture) + '\t' + TopRightX.ToString("F6", CultureInfo.InvariantCulture) + '\t' + TopRightY.ToString("F6", CultureInfo.InvariantCulture));
+            text.AppendLine(PxMiddleX.ToString("F6", CultureInfo.InvariantCulture) + '\t' + PxMiddleY.ToString("F6", CultureInfo.InvariantCulture) + '\t' + MiddleX.ToString("F6", CultureInfo.InvariantCulture) + '\t' + MiddleY.ToString("F6", CultureInfo.InvariantCulture));
+            return text.ToString();
+        }
+
+    }
+}
